7e2fdfd2352dfd3d258d6b1d4818a951499d9114
marekbily <bily1marek@gmail.com>
2025-02-01 20:06:15 +0100

    Refactor GUI and camera code, add h5 dataset file type, successful tests done

--- src/GUI/main.py
+++
@@ -1,9 +0,0 @@
-from qtpy.QtWidgets import QApplication
-from rheed import Rheed
-
-if __name__ == "__main__":
-    app = QApplication([])
-    window = Rheed()
-    window.show()
-    app.exec_()
-    
