f548848174eee95704d3b5386b7929d76b66a7b6
marekbily <bily1marek@gmail.com>
2025-02-01 20:09:20 +0100

    Refactor code for improved performance

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from qtpy.QtWidgets import QApplication
+from GUI.rheed import Rheed
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = Rheed()
+    window.show()
+    app.exec_()
+    
